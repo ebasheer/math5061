84ffd8418d403cad49ea7f73bf12a589bda20a23
Ershaad Basheer <ershaad.a@gmail.com>
2016-09-10 19:53:13 -0400

    initial. define string

---
+++ week2/charhisto/charhisto.py
@@ -0,0 +1,14 @@
+""" Program to count number of occurrences of characters"""
+
+instr = "Twas the night before start-up and all through the net "\
+        "not a packet was moving; no bit nor octet. "\
+        "The engineers rattled their cards in despair "\
+        "hoping a bad chip would blow with a flare. "\
+        "The salesmen were nestled all snug in their beds, "\
+        "while visions of data nets danced in their heads. "\
+        "And I with my datascope tracings and dumps "\
+        "prepared for some pretty bad bruises and lumps. "\
+        "When out in the hall there arose such a clatter, "\
+        "I sprang from my desk to see what was the matter"
+
+print(instr)
