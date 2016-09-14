a6ca5f44c5b415cc85eb81ef21f54ee6ec1a88c7
Ershaad Basheer <ershaad.a@gmail.com>
2016-09-14 13:42:37 -0400

    add charcount example

---
+++ week3/charcount/charhisto_full.py
@@ -0,0 +1,40 @@
+""" Program to count number of occurrences of characters"""
+
+instr = "Twas the night before start-up and all through the net "\
+        "not a packet was moving; no bit nor octet. "\
+        "The engineers rattled their cards in despair "\
+        "hoping a bad chip would blow with a flare. "\
+        "The salesmen were nestled all snug in their beds, "\
+        "while visions of data nets danced in their heads. "\
+        "And I with my datascope tracings and dumps "\
+        "prepared for some pretty bad bruises and lumps. "\
+        "When out in the hall there arose such a clatter, "\
+        "I sprang from my desk to see what was the matter"
+
+
+hist = {}
+for char in instr.lower():
+    if char.isalpha():
+        if char not in hist:
+            hist[char] = 1
+        else:
+            hist[char] += 1
+
+for char, num in hist.items():
+    print(char,': ', '+'*num)
+    
+mystr = "prepared for some pretty bad bruises and lumps. "\
+        "When out in the hall there arose such a clatter, "\
+        "I sprang from my desk to see what was the matter"
+
+
+hist = {}
+for char in mystr.lower():
+    if char.isalpha():
+        if char not in hist:
+            hist[char] = 1
+        else:
+            hist[char] += 1
+
+for char, num in hist.items():
+    print(char,': ', '+'*num)
