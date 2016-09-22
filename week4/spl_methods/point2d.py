e74e3a9a7df981514d43ffb106728a78afc15659
Ershaad Basheer <ershaad.a@gmail.com>
2016-09-21 21:33:03 -0400

    week4 day 2

---
+++ week4/spl_methods/point2d.py
@@ -0,0 +1,12 @@
+class Point2D():
+
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def printxy(self):
+        print('x coord:',self.x)
+        print('y coord:',self.y)
+
+ainst = Point2D()
+print(ainst)
